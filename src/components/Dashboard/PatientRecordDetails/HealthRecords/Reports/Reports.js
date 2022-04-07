07698105c30f5a24af8f06cbe99f7e5fe59e29d9
zahin52 <zahinuddin5@gmail.com>
2022-04-07 16:30:57 +0600

    pdf viewer fixing

--- src/components/Dashboard/PatientRecordDetails/HealthRecords/Reports/Reports.js
+++ src/components/Dashboard/PatientRecordDetails/HealthRecords/Reports/Reports.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useWrapperHeight from "../../../../../CustomHooks/useWrapperHeight";
 import "./Reports.css";
+import $ from "jquery";
 
 export default function Reports() {
+	const data1 = ["RTPCR Report", "RTPCR Report", "RTPCR Report"];
+	const data2 = ["COVID19 Vaccin", "COVID19 Vaccin", "COVID19 Vaccin"];
 	const [StoptLoop, setStoptLoop] = React.useState(true);
+	const [Data, setData] = React.useState(data1);
 	const getInnerHeight = (elm) => {
 		var computed = getComputedStyle(elm),
 			padding =
@@ -23,41 +27,109 @@ export default function Reports() {
 		cardWrapper.style.height =
 			getInnerHeight(Parent) - getInnerHeight(Head) + "px";
 	};
-	// useWrapperHeight("healthrecords", "reportsHead", "listWrapper");
 	setTimeout(() => {
 		wrapperDynamicHeight("healthrecords", "reportsHead", "listWrapper");
 		setStoptLoop(false);
 	}, 2000);
+	const handleTabSwitch = () => {
+		const tabs = document.getElementsByClassName("tabtitle");
+		$(".tabtitle").on("click", (e) => {
+			// console.log(e);
+			// const target = e.target;
+			// $(target).addclass("active");
+			$(".tabtitle").each((i, obj) => {
+				if ($(obj).hasClass("active")) {
+					$(obj).removeClass("active");
+				}
+			});
+			$(e.target).addClass("active");
+			// console.log($(e.target).addClass("active"));
+		});
+	};
+	useEffect(() => {
+		handleTabSwitch();
+	});
 	return (
 		<div className='reports'>
-			<div className='d-flex  justify-content-between align-items-center reportsHead'>
-				<div>
-					<span className='tabtitle me-3'>Healthrecords</span>
-					<span className='tabtitle'>Healthrecords</span>
+			<div className='reportsHead'>
+				<div className='d-flex  justify-content-between align-items-center '>
+					<div>
+						<span
+							onClick={() => setData(data1)}
+							className='tabtitle active me-3'>
+							Healthrecords
+						</span>
+						<span
+							onClick={() => setData(data2)}
+							className='tabtitle'>
+							Healthcamps Attended
+						</span>
+					</div>
+					<div>
+						<span className='addBtn d-flex align-items-center'>
+							<span>
+								<img
+									src='/assets/images/record.svg'
+									alt=''
+									className='img-fluid'
+								/>
+							</span>
+							<span className='ms-2'>Add healthcamp</span>
+						</span>
+					</div>
 				</div>
-				<div>
-					<button>Add healthcamp</button>
+				<div className='d-flex mt-2'>
+					<div className='col-3 tableHead'>Reference Name</div>
+					<div className='col-3 tableHead'>Type</div>
+					<div className='col-3 tableHead'>Date</div>
+					<div className='col-3 tableHead'>action</div>
 				</div>
-				{/* <div className='d-flex mt-2'>
-					<div className='col-3'>RTPCR Report</div>
-					<div className='col-3'>Report</div>
-					<div className='col-3'>22 mar 2021</div>
-					<div className='col-3'>action</div>
-				</div> */}
 			</div>
 			<div className='listWrapper pt-2 ' style={{ overflowY: "scroll" }}>
 				{StoptLoop ? (
 					""
 				) : (
 					<span>
-						{[
-							1, 1, 1, 1, 1, 1, 2, 4, 5, 23, 54, 4, 54, 3, 3, 2,
-						].map((item, ind) => (
-							<div className='d-flex'>
-								<div className='col-3'>RTPCR Report</div>
-								<div className='col-3'>Report</div>
-								<div className='col-3'>22 mar 2021</div>
-								<div className='col-3'>action</div>
+						{Data.map((item, ind) => (
+							<div className='d-flex my-2 listItem align-items-center'>
+								<div className='col-3 name'>{item}</div>
+								<div className='col-3 type'>Report</div>
+								<div className='col-3 date'>22 mar 2021</div>
+								<div className='col-3 actionBtn d-flex justify-content-center'>
+									<span>
+										<img
+											style={{
+												width: "2.5rem",
+												cursor: "pointer",
+											}}
+											src='/assets/images/View.svg'
+											alt=''
+											className='img-fluid'
+										/>
+									</span>
+									<span className='mx-2'>
+										<img
+											style={{
+												width: "2.5rem",
+												cursor: "pointer",
+											}}
+											src='/assets/images/edit.svg'
+											alt=''
+											className='img-fluid'
+										/>
+									</span>
+									<span>
+										<img
+											style={{
+												width: "2.5rem",
+												cursor: "pointer",
+											}}
+											src='/assets/images/download.svg'
+											alt=''
+											className='img-fluid'
+										/>
+									</span>
+								</div>
 							</div>
 						))}
 					</span>
